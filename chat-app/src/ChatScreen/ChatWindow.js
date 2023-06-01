72654aa23643c464df9bd45ffc98ba15b1e3ddff
Yuval-Silver <109355151+Yuval-Silver@users.noreply.github.com>
2023-06-01 11:42:36 +0300

    Added check to generate message in active chat only if the chat has been selected

--- chat-app/src/ChatScreen/ChatWindow.js
+++ chat-app/src/ChatScreen/ChatWindow.js
@@ -8,59 +8,62 @@ export function ChatWindow(props) {
 	const [inputText, setInputText] = useState("");
 	const [sendClicked, setSendClicked] = useState(false);
 	const [messages, setMessages] = useState(<div id="chatArea"></div>);
-	
+
 	const activeChat = props.activeChat;
-	
+
 	// Effect to update the messages in a given chat.
 	useEffect(() => {
-		const text = inputText;
-		const url = "http://localhost:5000/api/Chats/" + activeChat["chatId"] + "/Messages/";
-		const request = {
-			'method': 'POST',
-			'headers': {
-				'Content-Type': 'application/json',
-				'Authorization': props.token
-			},
-			'body': JSON.stringify({"msg": text})
-		};
-		fetch(url, request)
-			.catch(() => {})
-			.finally(() => {
-				setSendClicked(false);
-				generateMessages(activeChat, setMessages, props.token);
-				setInputText("");
-			});
+		//Check if and active chat has been selected
+		if (Object.keys(activeChat).length !== 0) {
+			const text = inputText;
+			const url = "http://localhost:5000/api/Chats/" + activeChat["chatId"] + "/Messages/";
+			const request = {
+				'method': 'POST',
+				'headers': {
+					'Content-Type': 'application/json',
+					'Authorization': props.token
+				},
+				'body': JSON.stringify({ "msg": text })
+			};
+			fetch(url, request)
+				.catch(() => { })
+				.finally(() => {
+					setSendClicked(false);
+					generateMessages(activeChat, setMessages, props.token);
+					setInputText("");
+				});
+		}
 	}, [sendClicked, activeChat, inputText, props.token]);
-	
+
 	// Effect to make sure that the scroll bar sticks to the bottom.
 	useEffect(() => {
 		const chatArea = document.getElementById("chatArea");
 		if (chatArea)
 			chatArea.scrollTo(0, chatArea.scrollHeight);
 	}, [messages]);
-	
+
 	if (Object.keys(activeChat).length === 0) {
 		return <div id="emptyChatBox"></div>;
 	}
-	
+
 	const chatImg = activeChat.profilePic;
 	return (
-	<div id="chatBox">
-		<div id="title">
-			<img id="profileImg" src={chatImg} alt="Raccoon" />
-			<div id="userInfo">
-				<span id="name">{activeChat["username"]}</span>
-				<span id="status">Online</span>
+		<div id="chatBox">
+			<div id="title">
+				<img id="profileImg" src={chatImg} alt="Raccoon" />
+				<div id="userInfo">
+					<span id="name">{activeChat["username"]}</span>
+					<span id="status">Online</span>
+				</div>
 			</div>
+			{messages}
+			<ChatTextField
+				setInputText={setInputText}
+				onSend={() => {
+					setSendClicked(true);
+				}}
+			/>
 		</div>
-		{messages}
-		<ChatTextField
-			setInputText={setInputText}
-			onSend={() => {
-				setSendClicked(true);
-			}}
-		/>
-	</div>
 	);
 }
 
@@ -69,7 +72,7 @@ async function generateMessages(activeChat, setMessages, token) {
 		setMessages(<div id="chatArea"></div>);
 		return;
 	}
-	
+
 	const url = "http://localhost:5000/api/Chats/" + activeChat.chatId + "/Messages/";
 	const res = await fetch(url, {
 		'method': 'GET',
@@ -79,7 +82,7 @@ async function generateMessages(activeChat, setMessages, token) {
 		},
 	});
 	const messagesJson = await res.json();
-	
+
 	// Convert the JSON into a message array.
 	const messages = messagesJson.map(msg => {
 		return {
@@ -88,10 +91,10 @@ async function generateMessages(activeChat, setMessages, token) {
 			direction: (msg.sender.username === activeChat.username) ? "left" : "right",
 		};
 	});
-	
+
 	// Sort messages by the date.
 	messages.sort((a, b) => a.timestamp < b.timestamp ? -1 : a.timestamp > b.timestamp ? 1 : 0);
-	
+
 	// Add the elements to the array.
 	const messageComps = [];
 	for (let i = 0; i < messages.length; i++) {
@@ -101,10 +104,10 @@ async function generateMessages(activeChat, setMessages, token) {
 				direction={messages[i].direction}
 				timestamp={`${date.getHours()}:${date.getMinutes()}`}
 				key={i.toString()}>
-					{messages[i].message}
+				{messages[i].message}
 			</SpeechBubble>
 		);
 	}
-	
+
 	setMessages(<div id="chatArea">{messageComps}</div>);
 }

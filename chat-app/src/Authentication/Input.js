f7e76ffdaf4496f1f8b9658ce569482f9088dab5
NubPaws <dxroomram@gmail.com>
2023-06-01 17:44:51 +0300

    Fixed bootstrap removal bug indentation

--- chat-app/src/Authentication/Input.js
+++ chat-app/src/Authentication/Input.js
@@ -4,9 +4,11 @@ export function Input(props) {
     return (
         <div className={props.className}>
             <label className="fieldLabel" htmlFor={props.id}>{props.children}</label>
-            <input className="fieldInput" type={props.type}  name={props.name} id={props.id} 
-                placeholder={props.placeholder} onChange={props.onChange} required/>
-                <span id={props.errorMessageId}></span>
+            <div className="fieldInputContainer">
+                <input className="fieldInput" type={props.type}  name={props.name} id={props.id} 
+                    placeholder={props.placeholder} onChange={props.onChange} required/>
+                <div id={props.errorMessageId}></div>
+            </div>
         </div>
     );
 }

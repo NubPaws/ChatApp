bdfe0291edd305584cafed1051341e4b6dc95871
Yuval-Silver <109355151+Yuval-Silver@users.noreply.github.com>
2023-06-15 11:37:11 +0300

    Implemented Image Selection in registration screen

--- android-app/app/src/main/java/com/example/androidapp/RegisterActivity.java
+++ android-app/app/src/main/java/com/example/androidapp/RegisterActivity.java
@@ -1,8 +1,23 @@
 package com.example.androidapp;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.PickVisualMediaRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +25,36 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        ImageView imPreviewPicture = findViewById(R.id.imPreviewPicture);
+
+        ActivityResultLauncher<Intent> imagePickerActivity = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        if (data != null && data.getData() != null) {
+                            Uri selectedImageUri = data.getData();
+                            Bitmap selectedImageBitmap = null;
+                            try {
+                                selectedImageBitmap = MediaStore.Images.Media.getBitmap(
+                                        this.getContentResolver(),
+                                        selectedImageUri);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            imPreviewPicture.setImageBitmap(selectedImageBitmap);
+                        }
+                    }
+                });
+
+        Button btnSelectPicture = findViewById(R.id.btnSelectPicture);
+        btnSelectPicture.setOnClickListener(view -> {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            imagePickerActivity.launch(intent);
+        });
     }
+
 }
